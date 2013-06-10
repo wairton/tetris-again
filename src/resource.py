86e5750bd0ba55ecb5216b083189184267ece5ca
wairton de abreu <wairton@link.(none)>
2013-06-10 18:15:19 -0300

    Adicionado arquivo de recursos.

---
+++ src/resource.py
@@ -0,0 +1,18 @@
+import os
+from config import IMG_PATH
+
+#menu
+IMG_LOGO = os.path.join(IMG_PATH, 'logo_.png')
+IMG_BUTTON = os.path.join(IMG_PATH, 'menu_.png')
+IMG_BUTTON_SEL = os.path.join(IMG_PATH, 'menu_selected_2.png')
+RECORD_FILE = os.path.join(GAME_PATH, 'records.conf')
+
+#Block files
+BLACK_BLOCK = os.path.join(IMG_PATH, 'block_black.png')
+BLUE_BLOCK = os.path.join(IMG_PATH, 'block_blue.png')
+GREEN_BLOCK = os.path.join(IMG_PATH, 'block_green.png')
+INDIGO_BLOCK = os.path.join(IMG_PATH, 'block_indigo.png')
+LIGHT_BLUE_BLOCK = os.path.join(IMG_PATH, 'block_light_blue.png')
+ORANGE_BLOCK = os.path.join(IMG_PATH, 'block_orange.png')
+RED_BLOCK = os.path.join(IMG_PATH, 'block_red.png')
+YELLOW_BLOCK = os.path.join(IMG_PATH, 'block_yellow.png')
